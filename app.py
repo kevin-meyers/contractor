ee3ec80759c00d7d2e8fd06195b8c69d1c5bbd83
Kevin <kevinm1776@gmail.com>
2019-10-04 14:13:45 -0700

    Testing MongoDB on heroku.

--- app.py
+++ app.py
@@ -1,29 +1,23 @@
-import json
 import os
 
-from flask import Flask, render_template, request
+from flask import Flask
 from pymongo import MongoClient
-
-
-client = MongoClient("mongodb+srv://Kevin:hoiiaATXovVdCW54@cluster0-gwjng.mongodb.net/admin?retryWrites=true&w=majority")
-db = client.Contractor
-items = db.items
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
 
-if not items.find_one():
-    from add_items import add_items
-    add_items()
+host = (
+    f'mongodb+srv://{os.getenv("MONGODB_USER")}:{os.getenv("MONGODB_PASSWORD")}@'
+    'cluster0-gwjng.mongodb.net/admin?retryWrites=true&w=majority'
+)
 
+client = MongoClient(host=host)
+
+db = client.Contractor
+items = db.items
+
+print(items.find_one())
 @app.route('/')
-def home():
-    return render_template('base.html', items=items.find())
-
-@app.route('/items/<item_id>')
-def show_item(item_id):
-    item = item
-
-
-if __name__ == '__main__':
-  app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
-  
+def test():
+    return items.find_one()['name']
