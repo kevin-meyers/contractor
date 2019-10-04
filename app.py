31da9ce2cc4cc7f5da10f1f158eac77e3860e56f
Kevin <kevinm1776@gmail.com>
2019-10-04 12:07:50 -0700

    Changed port.

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import Flask, render_template, request
 from pymongo import MongoClient
 
 host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Contractor')
-client = MongoClient(host=f'{host}')
+client = MongoClient(host=host)
 db = client.Contractor
 items = db.items
 
@@ -19,3 +19,7 @@ def home():
 @app.route('/items/<item_id>')
 def show_item(item_id):
     item = item
+
+
+if __name__ == '__main__':
+  app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
