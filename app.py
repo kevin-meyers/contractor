2768052bc4eff16ef12c999232b0e3fac0217863
Kevin <kevinm1776@gmail.com>
2019-10-05 16:17:12 -0700

    Setup heroku local and config vars.

--- app.py
+++ app.py
@@ -1,28 +1,39 @@
+import json
 import os
 
-from flask import Flask
+from flask import Flask, render_template, request
 from pymongo import MongoClient
-from dotenv import load_dotenv
-load_dotenv()
 
-app = Flask(__name__)
+if os.environ.get('IS_PROD', None):
+    from dotenv import load_dotenv
+    load_dotenv()
 
-host = (
+
+client = MongoClient(
     f'mongodb+srv://{os.getenv("MONGODB_USER")}:{os.getenv("MONGODB_PASSWORD")}@'
     'cluster0-gwjng.mongodb.net/admin?retryWrites=true&w=majority'
 )
 
-client = MongoClient(host=host)
-
 db = client.Contractor
 items = db.items
 
-print(items.find_one())
+app = Flask(__name__)
+
+
+#if not items.find_one():
+#    from add_items import add_items
+#    add_items()
+
 @app.route('/')
-def test():
-    return items.find_one()['name']
+def home():
+    return os.environ.get('MONGODB_PASSWORD', 'no password')
+#    return render_template('base.html', items=items.find())
+
+#@app.route('/items/<item_id>')
+#def show_item(item_id):
+#    item = item
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
-
+  app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
+  
